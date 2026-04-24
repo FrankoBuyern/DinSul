d65f89d3fd3de146366902e42db9d95de5f902f6
FrankoBuyern <skaryx@yandex.ru>
2026-04-24 20:35:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AppConfig config = ConfigLoader.load(AppConfig.class);
+        System.out.println(config);
+    }
+}
